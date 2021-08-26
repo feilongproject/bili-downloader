e39c33571703f17c098c4d88484bfac3a007cc41
飞龙project <flzs6137@gmail.com>
2021-08-27 01:32:02 +0800

    优化逻辑，控制台已获取到cid，但仍未在前端实现功能

--- index.js
+++ index.js
@@ -8,26 +8,39 @@
     https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/video/videostream_url.md
     https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/bangumi/info.md
 */
-var choices=1
+const choices = 1
+const cookies = ""
 var url
-var pathname
-var videoInfo =new Object()
+var pageStatus
+
 let htmlheaders = new Headers()
 htmlheaders.set('Content-Type', 'text/html;charset=UTF-8')
 
 
 
 
-
-
-
-
 addEventListener("fetch", async event => {
-	event.respondWith(handleRequest(event.request))
+  //clear()
+  event.respondWith(Main(event.request))
 })
 
-async function coutInfo(){
-    return `-------------------<br>
+
+async function Main(request) {
+  {//init
+    url = new URL(request.url)
+  }
+
+  //return new Response(url.pathname +"   "+ aid)
+  if (url.searchParams.get("down") == "1") {
+    return await DownloadVideo(request)
+  } else {
+    return await GetInfoPage(request)
+  }
+
+}
+
+function coutInfo() {
+  return `-------------------<br>
     bilibili视频下载助手<br>
     作者 : 飞龙project<br>
     demo link1 : <a href="https://www.bilibili.com/video/BV1Ap4y1b7UC">https://www.bilibili.com/video/BV1Ap4y1b7UC</a><br>
@@ -43,7 +56,7 @@ async function coutInfo(){
     <button onclick="tiaozhuan()">跳转</button><br>
     <script type="text/javascript">
         var tiaozhuan = function(){
-            document.location.href ='/' + document.getElementById("www").value;
+            document.location.href ='/?abid=' + document.getElementById("www").value;
         }
     </script>
     -------------------<br>
@@ -52,300 +65,103 @@ async function coutInfo(){
     添加剧集下载<br>
     美化界面(没css真难看)<br>
     添加cookie，使能下载更高画质视频<br>
-    
 
-    
-    
     -------------------<br>`
 
 
 }
 
 
-/*
-    function coutVideoInfo($videoIdInfo){
-        $str = "-------------------<br>" . 
-        "video type: ";
+async function GetInfoPage(request) {
+  //let aid = url.searchParams.get('aid')
 
-        if( $videoIdInfo["type"] == 0 )
-            $str = $str . "no<br>";
-        elseif( $videoIdInfo["type"] == 1 )
-            $str = $str . "avideo<br>";
-        elseif( $videoIdInfo["type"] == 2 )
-            $str = $str . "bvideo<br>";
-        elseif( $videoIdInfo["type"] == 3 )
-            $str = $str . "ssanime<br>";
-        elseif( $videoIdInfo["type"] == 4 )
-            $str = $str . "epanime<br>";
-        elseif( $videoIdInfo["type"] == 5 )
-            $str = $str . "media<br>";
-        $str = $str . "id: " . $videoIdInfo["id"] . "<br>";
-        $str = $str . "-------------------<br>";
-        echo $str;
-    }
+  var videoId = url.searchParams.get('abid')
 
-    function ShowError($errcode){
-        echo "-------------------<br>" .
-        "发生了一些错误：<br>" .
-        $errcode . "<br>" .
-        "uri: " . $_SERVER['HTTP_HOST'] . "<br>" .
-        "url: " . $_SERVER['REQUEST_URI'] ."<br>" .
-        "-------------------<br>";//输出完整的url
-    }
-
-    /*
-    function DownloadVideo($VideoData){
-
-        $url = stristr($_SERVER['REQUEST_URI'],"url=");
-        $url = stristr($url,"http");
-        //$url = stristr(stristr($_SERVER['REQUEST_URI'],"url="),"http");
-
-        $p = stristr($_SERVER['REQUEST_URI'],"p=");
-        $p = stristr($p,"&",1);
-        $p = substr($p,2);
-
-        $tittle = $VideoData["data"]["title"];
-
-        
-        
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/vnd.android.package-archive');
-        header('Content-Disposition: attachment; filename=哔哩哔哩.p'. $p . ".". $tittle . ".flv");
-        header('Content-Transfer-Encoding: binary');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
-        set_time_limit(0);
-        ini_set('max_execution_time', '0');
-        
-        $UserAgent = 'Mozilla/5.0 (Windows NT 6.1; rv:49.0) Gecko/20100101 Firefox/49.0';
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-        curl_setopt($ch, CURLOPT_REFERER, "https://api.bilibili.com/");
-        curl_setopt($ch, CURLOPT_USERAGENT, $UserAgent);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 0);
-        curl_setopt($ch, CURLOPT_BUFFERSIZE, 20971520);
-        $flag=0;
-        curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($ch ,$str) use (&$flag){
-            $len = strlen($str);
-            $flag++;
-            echo $str;
-
-            return $len;
-        });
-        $output = curl_exec($ch);
-        /*
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/vnd.android.package-archive');
-        header('Content-Disposition: attachment; filename=' . "hello.flv");
-        header('Content-Transfer-Encoding: binary');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
-        set_time_limit(0);
-        ini_set('max_execution_time', '0');
-
-        $curl=curl_init();
-        curl_setopt($curl, CURLOPT_URL, $LinkStr);//设置抓取的url
-        curl_setopt($curl, CURLOPT_REFERER, "https://api.bilibili.com/");
-        curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
-        curl_setopt($curl, CURLOPT_HEADER, 0);//设置头文件的信息作为数据流输出
-
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);//设置获取的信息以文件流的形式返回，而不是直接输出。
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 0);
-        //curl_setopt($curl, CURLOPT_ENCODING, 'gzip,deflate');  //gzip压缩内容
-        curl_setopt($curl, CURLOPT_WRITEFUNCTION, function ($curl, $buffer) {
-            echo $buffer;
-            return strlen($buffer);
-        });
-        curl_exec($curl);//执行命令
+  {
+    //console.log(videoId)
+    //console.log(url)
+    console.log()
+    console.log()
+  }
 
 
-        //echo curl_getinfo($curl,CURLINFO_HTTP_CODE) . "<br><br>"; //输出请求状态码
+  if (videoId ?.includes("bilibili.com") ){
+    videoId = (new URL(videoId)).pathname//   ||  /video/BV1Ap4y1b7UC  || /video/BV1Ap4y1b7UC
 
-        curl_close($curl);//关闭URL请求
+  }
 
-        return ;*/
-    /*
-    }
-    /*
 
-    function GetPlayLink($VideoData,$videoIdInfo){
-        $url1="http://api.bilibili.com/x/player/playurl";
-        //$url2="http://api.bilibili.com/pgc/view/web/season";
-        //$url3="http://api.bilibili.com/pgc/review/user";
 
-        if($videoIdInfo["type"]==1 || $videoIdInfo["type"]== 2 ){
-            $url=$url1."?avid=" . $VideoData["data"]["aid"];
-            echo "-------------------<br>
-            视频标题：" . $VideoData["data"]["title"] . "<br>
-            ----------<br>";
 
-            for($i=0;$i<$VideoData["data"]["videos"];$i++){
-                $FinUrl = $url . "&cid=" . $VideoData["data"]["pages"][$i]["cid"];
-                $VideoLinkData = GetCurl($FinUrl);
+  var videoApiUrl = "http://api.bilibili.com/x/web-interface/view?"
 
-                $VideoLinkUrl = $VideoLinkData["data"]["durl"][0]["url"];
-                echo "分P标题：" . $VideoData["data"]["pages"][$i]["part"] . "<br>
-                下载：<button
-                    onclick="window.open('/download.php" . 
-                    "?p=". $i .
-                    "&bvid=" . $VideoData["data"]["bvid"] .
-                    "&url=" . $VideoLinkUrl . "')">
-                        第". ($i+1) . "P
-                </button>
-                <a href="/download.php?
-                    p=". $i .
-                    "&bvid=" . $VideoData["data"]["bvid"] .
-                    "&url=" . $VideoLinkUrl . "')">
-                        第". ($i+1) . "P
-                </a>
-            <br>";
+  //http://api.bilibili.com/x/web-interface/view?bvid=BV1Ap4y1b7UC
+  //http://api.bilibili.com/x/web-interface/view?aid=170001
+  if (stristr(videoId, "/"))
+    videoId = videoId.split("/")[videoId.split("/").length - 1]//把videoId彻底分割成avbv格式
+  if (stristr(videoId, "av") || stristr(videoId, "bv")) {//检测是否是av类视频
+    //console.log(videoId.split("/"))
+    console.log(videoId)
+    if (stristr(videoId, "av")) videoId = "aid=" + videoId.substr(2)
+    else videoId = "bvid=" + videoId
+  } else if (stristr(videoId, "ss") || stristr(videoId, "ep")) {//检测是否是ss类视频
+  }/*
+  else if (stristr($videoId, "md")) {//检测是否是md类视频
+    $videoIdInfo["type"] = 5;
+    $videoIdInfo["id"] = stristr($videoId, "md");
+  }
+  else {//若都不是则输出错误
+    ShowError("1");
+    exit();
+  }*/
 
-            }
+  var res = await fetch(videoApiUrl + videoId)
+  console.log(res)
+  var videoInfoJson = await res.json()
+  console.log(videoInfoJson)
+  var videoCid=videoInfoJson.data.cid
+  console.log(videoCid)
 
-        }
 
-        
-    }
+  return new Response(coutInfo(), { headers: htmlheaders })
+  return new Response(3, { headers: htmlheaders })
 
-    */
-//const pathname = new URL(request.url).pathname
+  $videoIdInfo = GetIdAccUrl();
+  coutVideoInfo($videoIdInfo);
+  $VideoData = GetInfo($videoIdInfo);
+  GetPlayLink($VideoData, $videoIdInfo);
 
-function init(request){
-    url = new URL(request.url)
-    pathname=url.pathname
+
 }
 
-async function handleRequest(request) {
-    init(request)
-    
-    
-    
-    //return new Response(url.pathname +" 1")
 
+async function DownloadVideo() {
+  //echo "开始下载<br>";
+  //echo stristr($_SERVER['REQUEST_URI'],"http") . "<br>";
+  $videoIdInfo = GetIdAccUrl(0);
+  //echo $videoIdInfo["type"] . $videoIdInfo["id"];
+  DownloadVideo(GetInfo($videoIdInfo, 0))
 
-
-    if( pathname.includes("download.php?") ){//如果是下载链接
-        //echo "开始下载<br>";
-        //echo stristr($_SERVER['REQUEST_URI'],"http") . "<br>";
-        $videoIdInfo = GetIdAccUrl(0);
-        //echo $videoIdInfo["type"] . $videoIdInfo["id"];
-        DownloadVideo(GetInfo($videoIdInfo,0));
-    
-    }else{
-
-        return new Response(pathname,{headers :htmlheaders})
-
-        $videoIdInfo = GetIdAccUrl();
-        coutVideoInfo($videoIdInfo);
-        $VideoData = GetInfo($videoIdInfo);
-        GetPlayLink($VideoData,$videoIdInfo);
-        return new Response(await coutInfo(),{headers :htmlheaders})
-    }
-    
-    
-    /*else{
-        GetInfo($videoIdInfo);
-    }*/
 }
 
 
 
 
-async function GetVideoInfo(){
-
-    
 
 
-    if( stristr($videoId,"av") ){//检测是否是av类视频
-        $videoIdInfo["type"] = 1;
-        $videoIdInfo["id"] = stristr($videoId,"av");
-    }
-    else if( stristr($videoId,"bv") ){//检测是否是bv类视频
-        $videoIdInfo["type"] = 2;
-        $videoIdInfo["id"] = stristr($videoId,"bv");
-    }
-    else if( stristr($videoId,"ss") ){//检测是否是ss类视频
-        $videoIdInfo["type"] = 3;
-        $videoIdInfo["id"] = stristr($videoId,"ss");
-    }
-    else if( stristr($videoId,"ep") ){//检测是否是ep类视频
-        $videoIdInfo["type"] = 4;
-        $videoIdInfo["id"] = stristr($videoId,"ep");
-    }
-    else if( stristr($videoId,"md") ){//检测是否是md类视频
-        $videoIdInfo["type"] = 5;
-        $videoIdInfo["id"] = stristr($videoId,"md");
-    }
-    else{//若都不是则输出错误
-        ShowError("1")
-    }
-
-
-
-        $curl=curl_init();
-        curl_setopt($curl, CURLOPT_URL, $LinkStr);//设置抓取的url
-        curl_setopt($curl, CURLOPT_REFERER, "https://api.bilibili.com/");
-        //curl_setopt($curl, CURLOPT_HEADER, 1);//设置头文件的信息作为数据流输出
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);//设置获取的信息以文件流的形式返回，而不是直接输出。
-        $VideoData = curl_exec($curl);//执行命令
-        //echo curl_getinfo($curl,CURLINFO_HTTP_CODE) . "<br>"; //输出请求状态码
-        curl_close($curl);//关闭URL请求
-        $VideoData = json_decode($VideoData,true);
-        return $VideoData;
-
-
-
-
-
-        $url1="http://api.bilibili.com/x/web-interface/view";
-        $url2="http://api.bilibili.com/pgc/view/web/season";
-        $url3="http://api.bilibili.com/pgc/review/user";
-
-
-        if($videoIdInfo["type"]==1){
-            $url=$url1.'?'."aid=".substr($videoIdInfo["id"],2);
-        }elseif($videoIdInfo["type"]==2){
-            $url=$url1.'?'."bvid=".substr($videoIdInfo["id"],2);
-        }elseif($videoIdInfo["type"]==3){
-            $url=$url2.'?'."aid=".substr($videoIdInfo["id"],2);
-        }elseif($videoIdInfo["type"]==4){
-            $url=$url2.'?'."aid=".substr($videoIdInfo["id"],2);
-        }elseif($videoIdInfo["type"]==5){
-            $url=$url3.'?'."aid=".substr($videoIdInfo["id"],2);
-        }
-
-        if($show)
-            echo "视频详细api：" . $url."<br>";
-        
-        //print_r($VideoData);//显示获得的数据
-
-        $VideoData=GetCurl($url);
-
-        if($show)
-            for($i=0;$i<$VideoData["data"]["videos"];$i++){
-                echo "第" . ($i+1) . "个cid : " . $VideoData["data"]["pages"][$i]["cid"] . "<br>";
-            }
-        return $VideoData;
-    }
-
-function stristr (firststr, secondstr, bool) {
+function stristr(haystack, needle, bool) {
   let pos = 0
-  firststr += ''
-  pos = firststr.toLowerCase()
-    .indexOf((secondstr + '')
+  haystack += ''
+  pos = haystack.toLowerCase()
+    .indexOf((needle + '')
       .toLowerCase())
   if (pos === -1) {
     return false
   } else {
     if (bool) {
-      return firststr.substr(0, pos)
+      return haystack.substr(0, pos)
     } else {
-      return firststr.slice(pos)
+      return haystack.slice(pos)
     }
   }
 }
-/* */
