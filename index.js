ee9ff14bf208e70ecf326f78f4180179b54818ed
feilongproject <flzs6137@gmail.com>
2021-08-27 23:44:05 +0800

    beta版本更新

--- index.js
+++ index.js
@@ -7,42 +7,159 @@
     5->media
     https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/video/videostream_url.md
     https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/bangumi/info.md
+    https://bili.gq/?abid=av170001
+    http://bili.gq/download?cid=328118259&aid=972692871
+
 */
 const choices = 1
-const cookies = ""
+const cookies = "SESSDATA=123"
 var url
 var pageStatus
 
-let htmlheaders = new Headers()
-htmlheaders.set('Content-Type', 'text/html;charset=UTF-8')
+let htmlHeaders = new Headers()
+htmlHeaders.set('Content-Type', 'text/html;charset=UTF-8')
+let cookieHeaders = new Headers()
+cookieHeaders.set('Cookie', 'SESSDATA=13fae253%2C1645449241%2Ca84ed*81')
 
-
-
-
-addEventListener("fetch", async event => {
-  //clear()
-  event.respondWith(Main(event.request))
-})
 
 
 async function Main(request) {
   {//init
     url = new URL(request.url)
   }
-
+  //console.log(url)
   //return new Response(url.pathname +"   "+ aid)
-  if (url.searchParams.get("down") == "1") {
-    return await DownloadVideo(request)
+
+  if (url.pathname == "/") {
+    return new Response(infoPageHeader, { headers: htmlHeaders })
+  } else if (url.pathname == "/download") {
+    return await DownloadPage(request)
   } else {
-    return await GetInfoPage(request)
+    return await InfoPage(request)
   }
 
 }
 
-function coutInfo() {
-  return `-------------------<br>
+async function InfoPage(request) {
+  //let aid = url.searchParams.get('aid')
+
+  var videoId = url.searchParams.get('abid')
+  if (!videoId) {
+    videoId = "https:/" + url.pathname
+  }
+
+  {
+    console.log("videoId->" + videoId)
+    console.log(url)
+
+    console.log()
+  }
+
+
+  if (videoId ?.includes("bilibili.com") ){
+    videoId = (new URL(videoId)).pathname//   ||  /video/BV1Ap4y1b7UC  || /video/BV1Ap4y1b7UC
+
+  }
+
+
+  if (stristr(videoId, "/"))
+    videoId = videoId.split("/")[videoId.split("/").length - 1]//把videoId彻底分割成avbv格式
+  if (stristr(videoId, "av") || stristr(videoId, "bv")) {//检测是否是av类视频
+    //console.log(videoId.split("/"))
+    console.log(videoId)
+    if (stristr(videoId, "av")) videoId = "aid=" + videoId.substr(2)
+    else videoId = "bvid=" + videoId
+  } else if (stristr(videoId, "ss") || stristr(videoId, "ep")) {//检测是否是ss类视频
+  }/*
+  else if (stristr($videoId, "md")) {//检测是否是md类视频
+    $videoIdInfo["type"] = 5;
+    $videoIdInfo["id"] = stristr($videoId, "md");
+  }
+  else {//若都不是则输出错误
+    ShowError("1");
+    exit();
+  }*/
+
+
+  //http://api.bilibili.com/x/web-interface/view?bvid=BV1Ap4y1b7UC
+  //http://api.bilibili.com/x/web-interface/view?aid=170001
+  var videoApiUrl = "http://api.bilibili.com/x/web-interface/view?"
+  var res = await fetch(videoApiUrl + videoId)
+  //console.log(res)
+  var videoInfoJson = await res.json()
+  console.log(videoInfoJson)
+  //var videoCid = videoInfoJson.data.cid
+
+
+
+  for (i = 0; i < videoInfoJson.data.pages.length; i++) {
+    var cid = videoInfoJson.data.pages[i].cid
+    var infoPage = infoPageHeader
+      + "目标cid：" + cid + " (" + (i + 1) + "/" + videoInfoJson.data.pages.length + ")"
+      + "<button onclick=\"window.location.href = '/download\?cid=" + cid + "&aid=" + videoInfoJson.data.aid + "'\">跳转到下载详情页</button>"
+      + "<br>"
+    //console.log("cid(" + i + "/" + videoInfoJson.data.pages.length + "): " + cid)
+  }
+
+  return new Response(infoPage, { headers: htmlHeaders })
+
+
+  return new Response(3, { headers: htmlHeaders })
+
+
+
+}
+
+
+async function DownloadPage() {
+  console.log(url)
+  var downloadApi = "http://api.bilibili.com/x/player/playurl?"
+  var cid = url.searchParams.get('cid')
+  var aid = url.searchParams.get('aid')
+  console.log(cid)
+  console.log(aid)
+  var res = await fetch(downloadApi + "avid=" + aid + "&cid=" + cid, { headers: cookieHeaders })
+  var videoDownloadJson = await res.json()
+  console.log(videoDownloadJson)
+
+  var infoPage = infoPageHeader +
+    "当前页面cid: " + cid + "<br>" +
+    "当前画质: " + videoDownloadJson.data.format + "<br>"
+  /*for(i=0;i<videoDownloadJson.data.accept_description.length;i++){
+    infoPage+=videoDownloadJson.data.accept_description[i]+"<br>"
+  }*/
+
+  for (i = 0; i < videoDownloadJson.data.durl.length; i++) {
+    console.log(i + "  " + videoDownloadJson.data.durl.length + "  " + videoDownloadJson.data.durl[i])
+    infoPage +=
+      "<a href=" + videoDownloadJson.data.durl[i].url + ">画质:"
+      + videoDownloadJson.data.format +
+      "</a><br>"
+  }
+
+
+
+
+  return new Response(infoPage, { headers: htmlHeaders })
+}
+
+
+/*
+async function DownloadVideo() {
+  //echo "开始下载<br>";
+  //echo stristr($_SERVER['REQUEST_URI'],"http") . "<br>";
+  $videoIdInfo = GetIdAccUrl(0);
+  //echo $videoIdInfo["type"] . $videoIdInfo["id"];
+  DownloadVideo(GetInfo($videoIdInfo, 0))
+
+}
+*/
+
+
+var infoPageHeader = `-------------------<br>
     bilibili视频下载助手<br>
     作者 : 飞龙project<br>
+    GitHub开源项目仓库: <a href="https://github.com/feilongproject/bili-downloader">github.com/feilongproject/bili-downloader</a><br>
     demo link1 : <a href="https://www.bilibili.com/video/BV1Ap4y1b7UC">https://www.bilibili.com/video/BV1Ap4y1b7UC</a><br>
     demo link2 : <a href="/https://www.bilibili.com/video/BV1Ap4y1b7UC">http://bili.gq/https://www.bilibili.com/video/BV1Ap4y1b7UC</a><br>
     -------------------<br>
@@ -69,81 +186,6 @@ function coutInfo() {
     -------------------<br>`
 
 
-}
-
-
-async function GetInfoPage(request) {
-  //let aid = url.searchParams.get('aid')
-
-  var videoId = url.searchParams.get('abid')
-
-  {
-    //console.log(videoId)
-    //console.log(url)
-    console.log()
-    console.log()
-  }
-
-
-  if (videoId ?.includes("bilibili.com") ){
-    videoId = (new URL(videoId)).pathname//   ||  /video/BV1Ap4y1b7UC  || /video/BV1Ap4y1b7UC
-
-  }
-
-
-
-
-  var videoApiUrl = "http://api.bilibili.com/x/web-interface/view?"
-
-  //http://api.bilibili.com/x/web-interface/view?bvid=BV1Ap4y1b7UC
-  //http://api.bilibili.com/x/web-interface/view?aid=170001
-  if (stristr(videoId, "/"))
-    videoId = videoId.split("/")[videoId.split("/").length - 1]//把videoId彻底分割成avbv格式
-  if (stristr(videoId, "av") || stristr(videoId, "bv")) {//检测是否是av类视频
-    //console.log(videoId.split("/"))
-    console.log(videoId)
-    if (stristr(videoId, "av")) videoId = "aid=" + videoId.substr(2)
-    else videoId = "bvid=" + videoId
-  } else if (stristr(videoId, "ss") || stristr(videoId, "ep")) {//检测是否是ss类视频
-  }/*
-  else if (stristr($videoId, "md")) {//检测是否是md类视频
-    $videoIdInfo["type"] = 5;
-    $videoIdInfo["id"] = stristr($videoId, "md");
-  }
-  else {//若都不是则输出错误
-    ShowError("1");
-    exit();
-  }*/
-
-  var res = await fetch(videoApiUrl + videoId)
-  console.log(res)
-  var videoInfoJson = await res.json()
-  console.log(videoInfoJson)
-  var videoCid=videoInfoJson.data.cid
-  console.log(videoCid)
-
-
-  return new Response(coutInfo(), { headers: htmlheaders })
-  return new Response(3, { headers: htmlheaders })
-
-  $videoIdInfo = GetIdAccUrl();
-  coutVideoInfo($videoIdInfo);
-  $VideoData = GetInfo($videoIdInfo);
-  GetPlayLink($VideoData, $videoIdInfo);
-
-
-}
-
-
-async function DownloadVideo() {
-  //echo "开始下载<br>";
-  //echo stristr($_SERVER['REQUEST_URI'],"http") . "<br>";
-  $videoIdInfo = GetIdAccUrl(0);
-  //echo $videoIdInfo["type"] . $videoIdInfo["id"];
-  DownloadVideo(GetInfo($videoIdInfo, 0))
-
-}
-
 
 
 
@@ -165,3 +207,8 @@ function stristr(haystack, needle, bool) {
     }
   }
 }
+
+addEventListener("fetch", async event => {
+  //clear()
+  event.respondWith(Main(event.request))
+})
