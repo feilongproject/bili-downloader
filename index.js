f2ea3583384af2333bd6cbe245d16545fac3e15d
feilongproject <flzs6137@gmail.com>
2021-08-28 19:43:36 +0800

    独立页面文件，修改部分问题

--- index.js
+++ index.js
@@ -11,7 +11,6 @@
     http://bili.gq/download?cid=328118259&aid=972692871
 
 */
-const choices = 1
 const cookies = "SESSDATA=123"
 var url
 var pageStatus
@@ -19,7 +18,7 @@ var pageStatus
 let htmlHeaders = new Headers()
 htmlHeaders.set('Content-Type', 'text/html;charset=UTF-8')
 let cookieHeaders = new Headers()
-cookieHeaders.set('Cookie', 'SESSDATA=13fae253%2C1645449241%2Ca84ed*81')
+cookieHeaders.set('Cookie', cookies)
 
 
 
@@ -27,11 +26,11 @@ async function Main(request) {
   {//init
     url = new URL(request.url)
   }
-  //console.log(url)
+  console.log(url)
   //return new Response(url.pathname +"   "+ aid)
 
-  if (url.pathname == "/") {
-    return new Response(infoPageHeader, { headers: htmlHeaders })
+  if (!url.search) {
+    return new Response(await PageHeader(), { headers: htmlHeaders })
   } else if (url.pathname == "/download") {
     return await DownloadPage(request)
   } else {
@@ -91,22 +90,18 @@ async function InfoPage(request) {
   //var videoCid = videoInfoJson.data.cid
 
 
-
+  var infoPage = ""
   for (i = 0; i < videoInfoJson.data.pages.length; i++) {
     var cid = videoInfoJson.data.pages[i].cid
-    var infoPage = infoPageHeader
-      + "目标cid：" + cid + " (" + (i + 1) + "/" + videoInfoJson.data.pages.length + ")"
-      + "<button onclick=\"window.location.href = '/download\?cid=" + cid + "&aid=" + videoInfoJson.data.aid + "'\">跳转到下载详情页</button>"
+    var infoPage = infoPage + "目标cid：" + cid + " (" + (i + 1) + "/" + videoInfoJson.data.pages.length + ")"
+      + "<button class=\"downloadbutton\" onclick=\"window.location.href = '/download\?cid=" + cid + "&aid=" + videoInfoJson.data.aid + "'\">跳转到下载详情页</button>"
       + "<br>"
     //console.log("cid(" + i + "/" + videoInfoJson.data.pages.length + "): " + cid)
   }
-
+  infoPage = (await PageHeader())
+    .replaceAll("<!--INFOPAGE-->", infoPage)
   return new Response(infoPage, { headers: htmlHeaders })
-
-
-  return new Response(3, { headers: htmlHeaders })
-
-
+  //return new Response(3, { headers: htmlHeaders })
 
 }
 
@@ -121,10 +116,10 @@ async function DownloadPage() {
   var res = await fetch(downloadApi + "avid=" + aid + "&cid=" + cid, { headers: cookieHeaders })
   var videoDownloadJson = await res.json()
   console.log(videoDownloadJson)
-
-  var infoPage = infoPageHeader +
+  var infoPage = ""
+  infoPage = infoPage +
     "当前页面cid: " + cid + "<br>" +
-    "当前画质: " + videoDownloadJson.data.format + "<br>"
+    "当前最高画质: " + videoDownloadJson.data.format + "<br>"
   /*for(i=0;i<videoDownloadJson.data.accept_description.length;i++){
     infoPage+=videoDownloadJson.data.accept_description[i]+"<br>"
   }*/
@@ -139,58 +134,17 @@ async function DownloadPage() {
 
 
 
-
+  infoPage = (await PageHeader())
+    .replaceAll("<!--DOWNLOADPAGE-->", infoPage)
   return new Response(infoPage, { headers: htmlHeaders })
 }
 
 
-/*
-async function DownloadVideo() {
-  //echo "开始下载<br>";
-  //echo stristr($_SERVER['REQUEST_URI'],"http") . "<br>";
-  $videoIdInfo = GetIdAccUrl(0);
-  //echo $videoIdInfo["type"] . $videoIdInfo["id"];
-  DownloadVideo(GetInfo($videoIdInfo, 0))
+async function PageHeader() {
+  page = await fetch("https://raw.githubusercontent.com/feilongproject/bili-downloader/master/index.html")
+  return await page.text()
 
 }
-*/
-
-
-var infoPageHeader = `-------------------<br>
-    bilibili视频下载助手<br>
-    作者 : 飞龙project<br>
-    GitHub开源项目仓库: <a href="https://github.com/feilongproject/bili-downloader">github.com/feilongproject/bili-downloader</a><br>
-    demo link1 : <a href="https://www.bilibili.com/video/BV1Ap4y1b7UC">https://www.bilibili.com/video/BV1Ap4y1b7UC</a><br>
-    demo link2 : <a href="/https://www.bilibili.com/video/BV1Ap4y1b7UC">http://bili.gq/https://www.bilibili.com/video/BV1Ap4y1b7UC</a><br>
-    -------------------<br>
-    使用方法：<br>
-    方法1. 直接在网页将 " bilibili.com " 替换为" bili.gq"<br>
-    方法2. 在" bili.gq" 后添加 "av、bv号"<br>
-    方法3. 在" bili.gq" 后添加 "bilibili链接"<br>
-    方法4. 在下方输入bv av号/av bv链接进行跳转<br>
-    
-    <input type="text" id="www" >
-    <button onclick="tiaozhuan()">跳转</button><br>
-    <script type="text/javascript">
-        var tiaozhuan = function(){
-            document.location.href ='/?abid=' + document.getElementById("www").value;
-        }
-    </script>
-    -------------------<br>
-    TODO : <br>
-    添加番剧下载<br>
-    添加剧集下载<br>
-    美化界面(没css真难看)<br>
-    添加cookie，使能下载更高画质视频<br>
-
-    -------------------<br>`
-
-
-
-
-
-
-
 function stristr(haystack, needle, bool) {
   let pos = 0
   haystack += ''
