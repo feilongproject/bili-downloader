db269692a0e8fd86b2df57d0569736b602903415
flzs6137 <flzs6137@gmail.com>
2021-12-19 19:27:07 +0800

    [U]使用多线程获取番剧视频流

--- src/Page/Download/bangumi.ts
+++ src/Page/Download/bangumi.ts
@@ -12,28 +12,28 @@ import { GetFlvPage } from './GetFlv'
 export async function PageDownloadBangumi(cid: number, aid: number, dash: boolean): Promise<Response> {
 
     //console.log(`aid: ${aid}\ncid: ${cid}`)
-    var videoApiUrl: string
+    var params = `?avid=${aid}&cid=${cid}&fnval=${dash?"16":"0"}&fnver=0`
+    var videoApiUrl1 = `https://bili.lli.cx/pgc/player/web/playurl/${params}`
+    var videoApiUrl2 = `https://mysr.ellpew.xyz/pgc/player/web/playurl/${params}`
+    var videoApiUrl3 = `https://bilibili.myhosts.ml/pgc/player/web/playurl/${params}`
 
-    if (dash) {
+    var P1 = fetch(videoApiUrl1)
+    var P2 = fetch(videoApiUrl2)
+    var P3 = fetch(videoApiUrl3)
 
-        videoApiUrl = `https://mysr.ellpew.xyz/pgc/player/web/playurl?avid=${aid}&cid=${cid}`//only dash
-    } else {
-        videoApiUrl = `https://bilibili.myhosts.ml/pgc/player/web/playurl?avid=${aid}&cid=${cid}`//only flv
-    }
-
-    console.log(`fetching url: ${videoApiUrl}`)
-
-    var videoDownloadJson = await fetch(videoApiUrl, {
-        headers: cookieHeaders
+    var videoJson =await Promise.all([P1, P2, P3]).then(async res => {
+        return [await res[0].text(), await res[1].text(), await res[2].text(),]
+    }).then(text => {
+        return [JSON.parse(text[0]), JSON.parse(text[1]), JSON.parse(text[2]),]
     }).then(res => {
-        return res.text()
-    }).then(res => {
-        //console.log(res)
-        var ress = JSON.parse(res)
-        console.log(`code: ${ress.code}\nmessage: ${ress.message}`)
-        console.log(`result: ${JSON.stringify(ress.result)}`)
-
-        return ress.result
+        //console.log(`result: ${JSON.stringify(res[0].result)}`)
+        //console.log(`result: ${JSON.stringify(res[1].result)}`)
+        //console.log(`result: ${JSON.stringify(res[2].result)}`)
+        if ((res[0].result.type == "DASH") && dash)
+            return res[0].result
+        if ((res[1].result.type == "DASH") && dash)
+            return res[1].result
+        return res[2].result
     })
 
     //console.log(videoDownloadJson)
@@ -42,7 +42,7 @@ export async function PageDownloadBangumi(cid: number, aid: number, dash: boolea
         <span style="line-height: 100%;margin: 10px;">
             <div style="margin: 10px;">
                 <p>当前页面cid: ${cid}, aid: ${aid}</p>
-                <p>当前视频格式: ${videoDownloadJson.type}${videoDownloadJson.type == "FLV" ? `(bilibili已对当前下载格式进行速度限制)` : `当前格式会出现音视频分离现象，请谨慎下载`}</p>
+                <p>当前视频格式: ${videoJson.type}${videoJson.type == "FLV" ? `(bilibili已对当前下载格式进行速度限制)` : `当前格式会出现音视频分离现象，请谨慎下载`}</p>
         `
 
     var VideoSupportFormatsPage = `
@@ -53,10 +53,10 @@ export async function PageDownloadBangumi(cid: number, aid: number, dash: boolea
                         <th>是否需要vip</th>
                     </tr>
                 `
-    for (var i = 0; i < videoDownloadJson.support_formats.length; i++) {
-        var SupportFormat = videoDownloadJson.support_formats[i]
+    for (var i = 0; i < videoJson.support_formats.length; i++) {
+        var SupportFormat = videoJson.support_formats[i]
         //console.log(SupportFormat)
-        if ((SupportFormat.quality == videoDownloadJson.quality) && (videoDownloadJson.type != "DASH")) VideoSupportFormatsPage += `
+        if ((SupportFormat.quality == videoJson.quality) && (videoJson.type != "DASH")) VideoSupportFormatsPage += `
                     <tr>
                         <td><b>${SupportFormat.new_description}<b></td>
                         <td>${SupportFormat.need_login == true ? "是" : "否"}</td>
@@ -74,10 +74,10 @@ export async function PageDownloadBangumi(cid: number, aid: number, dash: boolea
     `
 
 
-    if (videoDownloadJson.type == "DASH")
-        infoPage += GetDashPage(videoDownloadJson.dash)
+    if (videoJson.type == "DASH")
+        infoPage += GetDashPage(videoJson.dash)
     else
-        infoPage += GetFlvPage(videoDownloadJson.durl)
+        infoPage += GetFlvPage(videoJson.durl)
 
 
     infoPage += `</div></span>`
