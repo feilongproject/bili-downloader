d9471f766409321234a96a6ea436fb587ba3c0a0
feilongproject <flzs6137@gmail.com>
2022-01-02 19:33:26 +0800

    [F]修复错误的提交cookie方式

--- src/var.sample.ts
+++ src/var.sample.ts
@@ -1,6 +1,6 @@
 //import {cookieHeaders} from '../../var'
 export const cookieHeaders = new Headers({
-    cookies: "",
+    "cookie": "",
     "accept": `text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9`,
     "accept-language": `zh-CN,zh;q=0.9`,
     "cache-control": `max-age=0`,
